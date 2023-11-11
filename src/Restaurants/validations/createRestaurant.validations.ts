29d0db0c10d09c35739cc987f1266585c80475c0
Mauro Hernandez <mauroDeveloper@hotmail.com>
2023-11-11 17:17:32 -0400

    cambios

--- src/Restaurants/validations/createRestaurant.validations.ts
+++ src/Restaurants/validations/createRestaurant.validations.ts
@@ -1,5 +1,6 @@
+import { type RequestHandler } from 'express';
 import validator from 'express-validator';
 
-const createRestaurant = [validator.check('name').notEmpty()];
+const createRestaurant: RequestHandler[] = [validator.check('name').notEmpty()];
 
 export default createRestaurant;
--- src/common/utils/commonValidator.ts
+++ src/common/utils/commonValidator.ts
@@ -1,6 +1,16 @@
-import { type NextFunction, type Request, type Response } from 'express';
+import {
+  type RequestHandler,
+  type NextFunction,
+  type Request,
+  type Response
+} from 'express';
 
-function commonValidator(validations = [], params: unknown): unknown {
+function commonValidator(
+  validations: RequestHandler[] = [],
+  params?: unknown
+): [
+  RequestHandler[],
+  () => {}] {
   return [validations, (req: Request, res: Response, next: NextFunction) => {}];
 }
 
